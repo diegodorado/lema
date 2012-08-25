0a52804ce05cac75c408b5957786f1e8f02370d4
diegodorado <diegodorado@gmail.com>
2012-08-24 19:25:13 -0600

    author

--- app/controllers/agenda_controller.rb
+++ app/controllers/agenda_controller.rb
@@ -1,5 +1,5 @@
 class AgendaController < ApplicationController
   def index
-    @posts = Post.where('draft=? and section in(?) and published_at<?', false, [:ag, :at], 30.days.ago).order('published_at DESC').page(params[:page]).per(10)
+    @posts = Post.where('draft=? and section in(?) and published_at>?', false, [:ag, :at], 30.days.ago).order('published_at DESC').page(params[:page]).per(10)
   end
 end
