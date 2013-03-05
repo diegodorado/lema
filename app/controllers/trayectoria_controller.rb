bde184b15412757939509822b23e4ca3704987f0
Diego Dorado <diegodorado@gmail.com>
2013-03-05 02:42:44 -0300

    updated query to fit PG

--- app/controllers/trayectoria_controller.rb
+++ app/controllers/trayectoria_controller.rb
@@ -1,6 +1,7 @@
 class TrayectoriaController < ApplicationController
   def index
-    @years = Post.select("DISTINCT strftime('%Y', published_at) as year").where('draft=? and section=? and published_at<?', false, :at, 30.days.ago).order('year desc').map{ |row| row.year.to_i }
+    #@years = Post.select("DISTINCT strftime('%Y', published_at) as year").where('draft=? and section=? and published_at<?', false, :at, 30.days.ago).order('year desc').map{ |row| row.year.to_i }
+    @years = Post.select("DISTINCT extract( year from timestamp published_at) as year").where('draft=? and section=? and published_at<?', false, :at, 30.days.ago).order('year desc').map{ |row| row.year.to_i }
     @current_year = (params[:year] || @years.first).to_i
     @posts = Post.for_year(@current_year).where('draft=? and section=? and published_at<?', false, :at, 30.days.ago).order('published_at DESC').page(params[:page]).per(10)
   end
